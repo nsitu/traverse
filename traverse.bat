f33d4516b0674749abfae1c028aeb8b5ed14c02b
nsitu <harold@nsitu.ca>
2018-09-24 16:09:04 -0400

    Add option for flipping slices
    Helps to prevent jagged edges in result.

--- traverse.bat
+++ traverse.bat
@@ -1,7 +1,20 @@
 @echo off
 
-IF "%~1"=="" ( 
-@echo Usage: traverse.bat [string video.mp4] [int sliceThickness] 
+IF "%~1"=="" (
+@echo ===========================================================
+@echo  TRAVERSE: a tool to generate a cross section from a video
+@echo ===========================================================
+@echo - All frames are centre-cropped to thin slices: e.g. 1920x2 pixels.
+@echo - Slices are re-assembled in sequence and saved as a panorama.
+@echo ------------------------
+@echo USAGE: traverse.bat [string inputFile] [int sliceThickness] [boolean doFlip]
+@echo - inputFile: any mp4 video
+@echo - sliceThickness: width in pixels of rectangular slice to be derived from each frame
+@echo - doFlip: whether or not to flip slices. May help correct directional jaggedness
+@echo ------------------------
+@echo OUTPUT: a TIFF image file is generated in the same folder
+@echo - The output file name will match that of the input file:
+@echo - e.g. 'content.mp4' results in 'content.tiff'
 exit /b
 )
 
@@ -15,19 +28,22 @@ SET sliceSize=%2
 IF "%~2"=="" ( SET sliceSize=2 )
 IF "%~2"=="0" ( SET sliceSize=2 )
 IF "%~2"=="1" ( SET sliceSize=2 )
+SET doFlip=
+IF "%~3"=="1" ( SET doFlip="-flip" )
+
 @echo Slice Thickness: %sliceSize% Pixels
 
-ffprobe -v error -select_streams v:0 -show_entries stream=width -of csv=s=x:p=0 %1 > tmpFile.txt 
-set /p theWidth= < tmpFile.txt 
-del tmpFile.txt 
+ffprobe -v error -select_streams v:0 -show_entries stream=width -of csv=s=x:p=0 %1 > tmpFile.txt
+set /p theWidth= < tmpFile.txt
+del tmpFile.txt
 
-ffprobe -v error -select_streams v:0 -show_entries stream=height -of csv=s=x:p=0 %1 > tmpFile.txt 
-set /p theHeight= < tmpFile.txt 
-del tmpFile.txt 
+ffprobe -v error -select_streams v:0 -show_entries stream=height -of csv=s=x:p=0 %1 > tmpFile.txt
+set /p theHeight= < tmpFile.txt
+del tmpFile.txt
 
-ffprobe -loglevel error -select_streams v:0 -show_entries stream_tags=rotate -of default=nw=1:nk=1 -i %1 > tmpFile.txt 
-set /p theRotation= < tmpFile.txt 
-del tmpFile.txt 
+ffprobe -loglevel error -select_streams v:0 -show_entries stream_tags=rotate -of default=nw=1:nk=1 -i %1 > tmpFile.txt
+set /p theRotation= < tmpFile.txt
+del tmpFile.txt
 
 SET /A ySlicePos =  %theHeight% / 2
 SET /A xSlicePos =  %theWidth% / 2
@@ -51,11 +67,11 @@ REM you still need to account for a 270 degree rotation.
   ) ELSE (
   @echo Slicing horizontally at Y Position: %ySlicePos%
   ffmpeg -i %inputFile% -vf "crop=%theWidth%:%sliceSize%:0:%ySlicePos%" -an %tempVideo%
-  ) 
+  )
  )
- @echo Smushing %tempVideo% into %outputFile%
- magick convert %tempVideo% -smush 0 -rotate 90 %outputFile% 
-)	
+ @echo Smushing %tempVideo% into %outputFile% using %doFlip% -smush
+ magick convert %tempVideo% %doFlip% -smush 0 -rotate 90 %outputFile%
+)
 
 
 IF %theHeight% gtr %theWidth% (
@@ -65,10 +81,13 @@ IF %theHeight% gtr %theWidth% (
  ) ELSE (
   @echo Slicing vertically at X Position: %xSlicePos%
   ffmpeg -i %inputFile% -vf "crop=2:%theHeight%:%xSlicePos%:0" -an %tempVideo%
-  @echo Smushing %tempVideo% into %outputFile%
-  magick convert %tempVideo% +smush 0 %outputFile%
+  @echo Smushing %tempVideo% into %outputFile% using %doFlip% +smush
+  magick convert %tempVideo% %doFlip% +smush 0 %outputFile%
  )
 )
 
 del %tempVideo%
+IF EXIST "doFlip" DEL /F "doFlip"
+IF EXIST "sliceThickness" DEL /F "sliceThickness"
+IF EXIST "inputFile" DEL /F "inputFile"
 @echo Finished.
